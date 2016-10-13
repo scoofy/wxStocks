d18c31a4b299ebd4dfc5a601e8b71a4422142861
scoofy <mschoolfield@gmail.com>
2016-10-12 18:14:30 -0700

    checks selected folder, and adds an information box

--- update_wxstocks.py
+++ update_wxstocks.py
@@ -3,10 +3,24 @@ import sys, os, wx, stat, hashlib, shutil
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 app = wx.App()
+info_label = "Update wxStocks"
+info_text = "You are about to update wxStocks.\nFirst, this update file should be run from your existing wxStocks folder.\nNext, you will select the folder of the new version of wxStocks.\nNote that the new version's folder should not be zipped.\nRegardless, none of your data files should be changed, nor should any of your user files be changed."
+info = wx.MessageBox(info_text, info_label, wx.OK | wx.ICON_INFORMATION)
 dialog = wx.DirDialog(None, "Choose a directory:",style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
 if dialog.ShowModal() == wx.ID_OK:
     newly_downloaded_path = dialog.GetPath()
+else:
+    sys.exit()
 dialog.Destroy()
+
+# check to see if selected folder is a wxStocks folder
+if not (os.path.isfile(newly_downloaded_path + "\\wxStocks.py") and os.path.isfile(newly_downloaded_path + "\\update_wxstocks.py")):
+    # if wxStocks.py or update_wxstocks.py don't exist
+    print "It appears you have not selected a wxStocks folder."
+    print "Please try again, selecting an unzipped version of wxStocks."
+    print "In the case of a change to the updating process, please check the wxStocks readme file in your new folder."
+    sys.exit()
+
 
 for root, dirs, files in os.walk(current_dir):
     for filename in files:
@@ -47,4 +61,4 @@ for root, dirs, files in os.walk(current_dir):
             if checksum_new != checksum_old:
                 print "UPDATING:", existing_path
                 shutil.copy(newly_downloaded_file_path, existing_path)
-                print "File successfully updated"
+                print '"' + filename + '"', "was successfully updated"
